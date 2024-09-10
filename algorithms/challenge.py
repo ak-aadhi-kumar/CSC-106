2bae82f1b45c3603e1de0db6fb5c01ee912c4c00
Aadhi Kumar <>
2024-09-10 00:14:52 -0400

    Added algorithms

---
+++ algorithms/challenge.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2024 Aadhi Kumar        
+#        
+# This program is free software: you can redistribute it and/or modify        
+# it under the terms of the GNU Affero General Public License as published        
+# by the Free Software Foundation, either version 3 of the License, or        
+# (at your option) any later version.        
+#        
+# This program is distributed in the hope that it will be useful,        
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        
+# GNU Affero General Public License for more details.        
+#        
+# You should have received a copy of the GNU Affero General Public License        
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+bread = input('What kind of bread do you want? ')
+pb = input('What kind of peanut butter do you want? ')
+jelly = input('What kind of jelly do you want? ')
+
+print(f'''
+First, put the {bread} on a plate.
+Next, spread the {jelly} on one slice of {bread}.
+Next, spread the {pb} on the other slice of {bread}.
+Finally, put them together.
+''')
+
